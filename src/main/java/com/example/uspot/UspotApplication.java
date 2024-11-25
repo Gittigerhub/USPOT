e146c32a595777d22c23c93bd52e789aa1cf9132
muziktiger <jh.24workspace@gmail.com>
2024-11-25 15:58:26 +0900

    개인프로젝트 생성

---
+++ src/main/java/com/example/uspot/UspotApplication.java
@@ -0,0 +1,13 @@
+package com.example.uspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UspotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uspot/UspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
